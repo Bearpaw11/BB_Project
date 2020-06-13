e389a4add677a6c77ba06abde932654a136317ea
Chris Behrens <chrisbehrens@yahoo.com>
2020-06-13 15:16:00 -0700

    added delete person functionality

--- src/App.js
+++ src/App.js
@@ -15,15 +15,7 @@ class App extends Component {
     
     
   
-    switchNameHandler = (newName) => {
-       this.setState({
-         persons: [
-         {name: newName, age: 8},
-         {name: "Frank", age: 3},
-         {name: "Tom", age: 5}
-        ]
-      });
-   };
+    
 
    nameChangedHandler =(event) => {
      this.setState( {
@@ -33,6 +25,12 @@ class App extends Component {
       { name: 'Stephanie', age: 26 }
      ]
     })
+   }
+
+   deletePersonHandler = (personIndex) => {
+     const persons = this.state.persons;
+     persons.splice(personIndex, 1);
+     this.setState({persons: persons})
    }
 
    togglePersonHandler = () => {
@@ -54,8 +52,9 @@ class App extends Component {
      if(this.state.showPersons) {
        persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person 
+              click = {() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age}/>
           })}
